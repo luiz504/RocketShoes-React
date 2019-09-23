72824ffe7aba7b71b3c5c497183108ab109c83ea
Luiz504 <luiz504@hotmail.com>
2019-09-23 14:44:40 -0300

    Base Routes config

---
+++ rocketshoes/README.md
@@ -0,0 +1,22 @@
+# WebPage using ReactJS E
+### Project Meaning
+Create a WebPage using learn about Flux Architecture on React Environment.
+
+----
+## Main page
+--
+## Cart page
+----
+
+## Libraries
+> create-react-app CLI
+
+> react-router-dom
+
+### Code STD
+> Eslist (AIRBNB)
+
+> Prettier
+
+> EditorConfig
+
--- rocketshoes/src/App.js
+++ rocketshoes/src/App.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Routes from './routes';
 
 export default function App() {
-  return <h1> Hello Readux</h1>;
+  return (
+    <BrowserRouter>
+      {/** <Header/> */}
+      <Routes />
+    </BrowserRouter>
+  );
 }
+/**
+ * Use the <BrowserRouter/> here to the <Header /> be able to access the routes
+ * <Header/> will be clicabel doing some functions and interaction with all app
+ */
